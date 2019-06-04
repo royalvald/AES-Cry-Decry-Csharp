06831f5297c357f0df2c18c01257caf6e19c21e7
Iris <royalvald@sina.com>
2019-06-04 14:22:44 +0800

    new changes

---
+++ SEDemo/SEDemo/testAlgorithm/saveInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDemo.testAlgorithm
+{
+    class saveInfo
+    {
+        public string random { set; get; }
+        public string result { set; get; }
+    }
+}
